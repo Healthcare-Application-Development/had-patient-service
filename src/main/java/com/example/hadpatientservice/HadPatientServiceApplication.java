ae3893e06f9c6438bb0cf1e2b95791da39507528
rohitraman <rohitraman9944@gmail.com>
2023-02-09 23:30:58 +0530

    first commit

---
+++ src/main/java/com/example/hadpatientservice/HadPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hadpatientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadPatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadPatientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hadpatientservice/HadPatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hadpatientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadPatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
